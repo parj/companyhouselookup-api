1180bd09f4f15ecb141a8a4f149d039959b156fd
Parj M <dork.github@gmail.com>
2020-05-03 17:07:05 +0100

    Updated to build a simple microservice

--- src/main/java/io/github/parj/companyhouselookup_api/App.java
+++ src/main/java/io/github/parj/companyhouselookup_api/App.java
@@ -1,12 +1,13 @@
 package io.github.parj.companyhouselookup_api;
 
+import io.github.parj.companyhouselookup.company.GetCompany;
+import io.github.parj.companyhouselookup.company.Search;
 import io.muserver.Method;
 import io.muserver.MuServer;
 import io.muserver.MuServerBuilder;
-
-import io.github.parj.companyhouselookup.company.GetCompany;
-import io.github.parj.companyhouselookup.company.Search;
+import io.muserver.MuStats;
 import kong.unirest.JsonNode;
+import kong.unirest.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,10 +29,29 @@ public class App {
                     JsonNode node = search.search(query);
                     response.contentType("application/json");
                     response.write(node.toString());
-                    System.out.println(query);
+                })
+                .addHandler(Method.GET, "/health", (request, response, pathParams) -> {
+                    response.contentType("application/json");
+                    response.write("{Status: UP}");
+                })
+                .addHandler(Method.GET, "/health/details", (request, response, pathParams) -> {
+                    MuStats stats = request.server().stats();
+                    long completedRequests = stats.completedRequests();
+                    long bytesRead = stats.bytesRead();
+                    long bytesSent = stats.bytesSent();
+                    response.contentType("application/json");
+                    JSONObject json = new JSONObject();
+                    json.put("Status", "UP");
+                    json.put("CompletedRequests", String.valueOf(completedRequests));
+                    json.put("BytesRead", String.valueOf(bytesRead));
+                    json.put("BytesSent", String.valueOf(bytesSent));
+                    response.write(json.toString());
                 })
                 .withHttpPort(8888)
                 .start();
         logger.info("Started server at " + server.uri());
+
+        if (System.getenv("API_COMPANYHOUSE_KEY") == null || System.getenv("API_COMPANYHOUSE_KEY").isEmpty() )
+            logger.error("Company house lookup API has not been set. To set - environment variable API_COMPANYHOUSE_KEY must be set");
     }
 }
