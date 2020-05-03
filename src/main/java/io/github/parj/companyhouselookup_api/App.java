b223d6b5ece9a6458cef39aaf8ac24ab2dc74f01
Parj M <dork.github@gmail.com>
2020-05-03 18:00:23 +0100

    Added context

--- src/main/java/io/github/parj/companyhouselookup_api/App.java
+++ src/main/java/io/github/parj/companyhouselookup_api/App.java
@@ -2,10 +2,7 @@ package io.github.parj.companyhouselookup_api;
 
 import io.github.parj.companyhouselookup.company.GetCompany;
 import io.github.parj.companyhouselookup.company.Search;
-import io.muserver.Method;
-import io.muserver.MuServer;
-import io.muserver.MuServerBuilder;
-import io.muserver.MuStats;
+import io.muserver.*;
 import kong.unirest.JsonNode;
 import kong.unirest.json.JSONObject;
 import org.slf4j.Logger;
@@ -16,37 +13,38 @@ public class App {
 
     public static void main(String[] args) {
         MuServer server = MuServerBuilder.httpServer()
-                .addHandler(Method.GET, "/company/get/{companyNumber}", (request, response, pathParams) -> {
-                    String companyNumber = pathParams.get("companyNumber");
-                    GetCompany getCompany = new GetCompany();
-                    JsonNode node = getCompany.get(companyNumber);
-                    response.contentType("application/json");
-                    response.write(node.toString());
-                })
-                .addHandler(Method.GET, "/company/search", (request, response, pathParams) -> {
-                    String query = request.query().get("q");
-                    Search search = new Search();
-                    JsonNode node = search.search(query);
-                    response.contentType("application/json");
-                    response.write(node.toString());
-                })
-                .addHandler(Method.GET, "/health", (request, response, pathParams) -> {
-                    response.contentType("application/json");
-                    response.write("{Status: UP}");
-                })
-                .addHandler(Method.GET, "/health/details", (request, response, pathParams) -> {
-                    MuStats stats = request.server().stats();
-                    long completedRequests = stats.completedRequests();
-                    long bytesRead = stats.bytesRead();
-                    long bytesSent = stats.bytesSent();
-                    response.contentType("application/json");
-                    JSONObject json = new JSONObject();
-                    json.put("Status", "UP");
-                    json.put("CompletedRequests", String.valueOf(completedRequests));
-                    json.put("BytesRead", String.valueOf(bytesRead));
-                    json.put("BytesSent", String.valueOf(bytesSent));
-                    response.write(json.toString());
-                })
+                .addHandler(ContextHandlerBuilder.context("lookup")
+                    .addHandler(Method.GET, "/company/get/{companyNumber}", (request, response, pathParams) -> {
+                        String companyNumber = pathParams.get("companyNumber");
+                        GetCompany getCompany = new GetCompany();
+                        JsonNode node = getCompany.get(companyNumber);
+                        response.contentType("application/json");
+                        response.write(node.toString());
+                    })
+                    .addHandler(Method.GET, "/company/search", (request, response, pathParams) -> {
+                        String query = request.query().get("q");
+                        Search search = new Search();
+                        JsonNode node = search.search(query);
+                        response.contentType("application/json");
+                        response.write(node.toString());
+                    })
+                    .addHandler(Method.GET, "/health", (request, response, pathParams) -> {
+                        response.contentType("application/json");
+                        response.write("{Status: UP}");
+                    })
+                    .addHandler(Method.GET, "/health/details", (request, response, pathParams) -> {
+                        MuStats stats = request.server().stats();
+                        long completedRequests = stats.completedRequests();
+                        long bytesRead = stats.bytesRead();
+                        long bytesSent = stats.bytesSent();
+                        response.contentType("application/json");
+                        JSONObject json = new JSONObject();
+                        json.put("Status", "UP");
+                        json.put("CompletedRequests", String.valueOf(completedRequests));
+                        json.put("BytesRead", String.valueOf(bytesRead));
+                        json.put("BytesSent", String.valueOf(bytesSent));
+                        response.write(json.toString());
+                    }))
                 .withHttpPort(8888)
                 .start();
         logger.info("Started server at " + server.uri());
